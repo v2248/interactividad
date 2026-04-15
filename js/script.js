b53852e40006367a96ffac033f71ba00879456f8
v2248 <vbetech@centro.edu.mx>
2026-04-14 20:09:07 -0600

    inicio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 2; 
+
+function suma(a, b) {
+const resultado = a + b; 
+alert(resultado);
+}
+
+function resta(a, b) {
+const resultado = a - b;
+alert(resultado);
+
+}
+
+document.addEventListener("click", function() {
+    document.body.style.backgroundColor = "yellow";
+});
